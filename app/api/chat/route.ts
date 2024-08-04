f1eaff1d339a56c1c1ebab87917c917b9f00806c
김진형 <bnt10@naver.com>
2024-08-04 16:24:59 +0900

    feat: add prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,12 @@
-import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
+
 import { createEdgeRuntimeAPI } from "@assistant-ui/react/edge";
 
 export const maxDuration = 30;
 
+const model = google("models/gemini-1.5-pro-latest");
+
 export const { POST } = createEdgeRuntimeAPI({
-  model: openai("gpt-4o"),
+  model: model,
+  system: `모든 응답을 한국어로 작성해 주세요. 설명에 대해서 프로그래밍에서 사용하는 변수 명으로 5개 추천해주세요`,
 });
